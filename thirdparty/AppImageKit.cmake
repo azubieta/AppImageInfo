bf9a0ae9deb36d84b510c846eae4308f1100ccf7
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-06-19 16:42:16 -0500

    Add AppImageKit as third party dependency

---
+++ thirdparty/AppImageKit.cmake
@@ -0,0 +1,23 @@
+find_package(AppImageKit QUIET)
+
+if (NOT AppImageKit_FOUND)
+    message(STATUS "Downloading and building AppImageKit")
+    include(ExternalProject)
+
+    ExternalProject_Add(AppImageKit
+            GIT_REPOSITORY https://github.com/AppImage/AppImageKit.git
+            GIT_TAG azubieta/fix_disable_tests_targets
+            GIT_SUBMODULES ""
+            CONFIGURE_COMMAND ${CMAKE_COMMAND} -G${CMAKE_GENERATOR} -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} <SOURCE_DIR>
+            BUILD_COMMAND make libappimage
+            INSTALL_COMMAND make install DESTDIR=<INSTALL_DIR>
+            )
+
+    ExternalProject_Get_Property(AppImageKit INSTALL_DIR)
+    set(AppImageKit_INSTALL_DIR ${INSTALL_DIR})
+
+    set(APPIMAGEKIT_LIBRARIES ${AppImageKit_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/lib/libappimage.so)
+    set(APPIMAGEKIT_INCLUDE_DIRS ${AppImageKit_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/include/)
+
+    install(FILES ${APPIMAGEKIT_LIBRARIES} DESTINATION lib COMPONENT desktop)
+endif (NOT AppImageKit_FOUND)
