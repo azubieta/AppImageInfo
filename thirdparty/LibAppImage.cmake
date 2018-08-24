c9fcd933c2d0c21d9d59becf5a8b862235ba2390
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-08-24 09:56:49 +0200

    Use new libappimage

---
+++ thirdparty/LibAppImage.cmake
@@ -0,0 +1,24 @@
+find_package(libappimage QUIET)
+
+if (NOT libappimage_FOUND)
+    message(STATUS "Downloading and building libappimage")
+    include(ExternalProject)
+
+    ExternalProject_Add(libappimage
+            GIT_REPOSITORY https://github.com/AppImage/libappimage.git
+            GIT_TAG master
+            GIT_SUBMODULES ""
+            CONFIGURE_COMMAND ${CMAKE_COMMAND} -G${CMAKE_GENERATOR} -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} <SOURCE_DIR>
+            BUILD_COMMAND make
+            INSTALL_COMMAND make install DESTDIR=<INSTALL_DIR>
+            )
+
+    ExternalProject_Get_Property(libappimage INSTALL_DIR BINARY_DIR)
+    message("libappimage INSTALL_DIR ${INSTALL_DIR} BINARY_DIR ${BINARY_DIR}")
+    set(LIBAPPIMAGE_INSTALL_DIR ${INSTALL_DIR})
+
+    set(LIBAPPIMAGE_LIBRARIES ${LIBAPPIMAGE_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/lib/libappimage.so)
+    set(LIBAPPIMAGE_INCLUDE_DIRS ${LIBAPPIMAGE_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/include/)
+
+    install(FILES ${LIBAPPIMAGE_LIBRARIES} DESTINATION lib COMPONENT desktop)
+endif (NOT libappimage_FOUND)
