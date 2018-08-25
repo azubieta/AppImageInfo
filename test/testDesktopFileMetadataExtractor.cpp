006ad3ed0f320a6b19be506176fb13d29a6737fa
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-08-25 18:58:55 +0200

    Add DesktopFileMetadataExtractor test.

--- test/testDesktopFileMetadataExtractor.cpp
+++ test/testDesktopFileMetadataExtractor.cpp
@@ -1,4 +1,18 @@
-//
-// Created by alexis on 8/25/18.
-//
+#include <gtest/gtest.h>
+#include <entities/DesktopFileMetadataExtractor.h>
 
+using namespace nlohmann;
+
+TEST(DesktopFileMetadataExtractorTest, getContent) {
+    DesktopFileMetadataExtractor extractor(TEST_DATA_DIR"/appimagetool.desktop");
+    json expected = R"({"Desktop Entry":{"Categories":["Development","Utils"],"Comment":{"default":"Tool to generate AppImages from AppDirs","es":"Herramienta para generar AppImages a partir de AppDirs"},"Exec":"appimagetool","Icon":{"default":"appimagetool"},"Name":{"default":"appimagetool"},"Terminal":"true","Type":"Application"},"id":"appimagetool.desktop"})"_json;
+
+    auto result = extractor.getContent();
+    ASSERT_EQ(result.dump(), expected.dump());
+}
+
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
