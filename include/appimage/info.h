ef82cbaa7baaf700f2e752c51913c56784f42b27
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-09-08 11:57:42 +0200

    Add 'appimage' as prefix of the public functions.

--- include/appimage/info.h
+++ include/appimage/info.h
@@ -5,22 +5,23 @@
 extern "C" {
 #endif
 
-void extract_desktop_file(const char *appimage_path, const char *output_path);
+void appimage_extract_desktop_file(const char *appimage_path, const char *output_path);
 
-void extract_appstream_file(const char *appimage_path, const char *output_path);
+void appimage_extract_appstream_file(const char *appimage_path, const char *output_path);
 
 /* Extracts the AppImage Icon.
  *
  * IMPORTANT: No image manipulation will be performed. This will only output the icon in the required
  * size if available at usr/share/icon/hicolor/<size> or it will fallback to the .DirIcon
  * */
-void extract_appinamge_icon_file(const char *appimage_path, const char *output_path, const char *size = "256");
+void appimage_extract_appinamge_icon_file(const char *appimage_path, const char *output_path, const char *size = "256");
 
-/* Combines contents from the application '.Desktop' file and the 'appstream.xml' file
+/* Combines contents from the application '.Desktop' file, the 'appstream.xml' file and information
+ * about the binary file.
  *
  * Returns: JSon formatted string
  * */
-char *extract_appinamge_info(const char *appimage_path);
+char *appimage_extract_info(const char *appimage_path);
 
 #ifdef __cplusplus
 }
--- src/lib/info.cpp
+++ src/lib/info.cpp
@@ -6,22 +6,22 @@
 #include "appimage/info.h"
 #include "../entities/FileMetadataExtractor.h"
 
-void extract_desktop_file(const char *appimage_path, const char *output_path) {
+void appimage_extract_desktop_file(const char *appimage_path, const char *output_path) {
     FileMetadataExtractor extractor(appimage_path);
     extractor.extractDesktopFile(output_path);
 }
 
-void extract_appstream_file(const char *appimage_path, const char *output_path) {
+void appimage_extract_appstream_file(const char *appimage_path, const char *output_path) {
     FileMetadataExtractor extractor(appimage_path);
     extractor.extractAppStreamFile(output_path);
 }
 
-void extract_appinamge_icon_file(const char *appimage_path, const char *output_path, const char *size) {
+void appimage_extract_appinamge_icon_file(const char *appimage_path, const char *output_path, const char *size) {
     FileMetadataExtractor extractor(appimage_path);
     extractor.extractIconFile(output_path, size);
 }
 
-char *extract_appinamge_info(const char *appimage_path) {
+char *appimage_extract_info(const char *appimage_path) {
     FileMetadataExtractor extractor(appimage_path);
     auto metadata = extractor.extractMetadata();
     char *out = strdup(metadata.dump().c_str());
