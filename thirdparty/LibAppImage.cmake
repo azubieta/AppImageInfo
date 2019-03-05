91ac781c2660cc2afd32c943b75256d1ba48fed5
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-03-05 09:29:25 -0600

    Install libappimage from travis install

--- thirdparty/LibAppImage.cmake
+++ thirdparty/LibAppImage.cmake
@@ -1,16 +1,14 @@
-find_package(libappimage QUIET)
-
-if (NOT TARGET libappimage)
+if(NOT USE_INSTALLED_LIBAPPIMAGE)
     include(ExternalProject)
 
     ExternalProject_Add(libappimage_project
-            GIT_REPOSITORY https://github.com/AppImage/libappimage.git
-            GIT_TAG master
-            GIT_SUBMODULES ""
-            GIT_SHALLOW 1
-            CONFIGURE_COMMAND ${CMAKE_COMMAND} -G${CMAKE_GENERATOR} -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} <SOURCE_DIR> -DCMAKE_INSTALL_LIBDIR=lib
-            BUILD_COMMAND make
-            INSTALL_COMMAND make install DESTDIR=<INSTALL_DIR>)
+        GIT_REPOSITORY https://github.com/AppImage/libappimage.git
+        GIT_TAG master
+        GIT_SUBMODULES ""
+        GIT_SHALLOW 1
+        CONFIGURE_COMMAND ${CMAKE_COMMAND} -G${CMAKE_GENERATOR} -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX} <SOURCE_DIR> -DCMAKE_INSTALL_LIBDIR=lib
+        BUILD_COMMAND make
+        INSTALL_COMMAND make install DESTDIR=<INSTALL_DIR>)
 
     ExternalProject_Get_Property(libappimage_project INSTALL_DIR)
     set(LIBAPPIMAGE_PROJECT_INSTALL_DIR ${INSTALL_DIR})
@@ -22,10 +20,11 @@ if (NOT TARGET libappimage)
     file(MAKE_DIRECTORY ${LIBAPPIMAGE_PROJECT_INSTALL_INCLUDE_DIR})
 
     set_target_properties(libappimage PROPERTIES
-            IMPORTED_LOCATION ${LIBAPPIMAGE_PROJECT_INSTALL_LIB_DIR}/libappimage.so
-            INTERFACE_INCLUDE_DIRECTORIES ${LIBAPPIMAGE_PROJECT_INSTALL_INCLUDE_DIR}
-            PREFIX "")
+        IMPORTED_LOCATION ${LIBAPPIMAGE_PROJECT_INSTALL_LIB_DIR}/libappimage.so
+        INTERFACE_INCLUDE_DIRECTORIES ${LIBAPPIMAGE_PROJECT_INSTALL_INCLUDE_DIR}
+        PREFIX "")
 
     add_dependencies(libappimage libappimage_project)
     install(FILES "${LIBAPPIMAGE_PROJECT_INSTALL_DIR}${CMAKE_INSTALL_PREFIX}/lib/libappimage.so" DESTINATION lib COMPONENT desktop)
-endif (NOT TARGET libappimage)
+endif(NOT USE_INSTALLED_LIBAPPIMAGE)
+
