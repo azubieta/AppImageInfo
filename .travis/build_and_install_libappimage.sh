58e7041f1c44df4bf2852217642e325c3a8543c4
Alexis Lopez Zubieta <azubieta90@gmail.com>
2019-03-06 09:48:38 -0600

    Use the libappimage master branch in builds

--- .travis/build_and_install_libappimage.sh
+++ .travis/build_and_install_libappimage.sh
@@ -7,8 +7,6 @@ pushd /tmp
 git clone https://github.com/AppImage/libappimage.git
 
 pushd libappimage
-
-git checkout add_missing_includes
 git submodule update --init --recursive
 
 cmake . -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DCMAKE_INSTALL_PREFIX=/usr -DUSE_SYSTEM_BOOST=On \
